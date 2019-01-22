7c13dfac38e1de06caba5bfbf142bd2b80b3ec7e
Marco <info@dreamfox.nl>
2019-01-22 11:51:39 +0100

    Update config.php

--- ow_includes/config.php
+++ ow_includes/config.php
@@ -1,23 +1,15 @@
 <?php
-
-define('OW_URL_HOME', 'https://develop.chamial.com/');
-
-define('OW_DB_HOST', 'localhost');
-define('OW_DB_PORT', null);
-define('OW_DB_USER', 'chamdev_develop');
-define('OW_DB_PASSWORD', 'develop');
-define('OW_DB_NAME', 'chamdev_develop');
-
-define('OW_DB_PREFIX', 'ch_');
-
+ 
 define('OW_DIR_USERFILES', OW_DIR_ROOT.'ow_userfiles'.DS);
 define('OW_DIR_STATIC', OW_DIR_ROOT.'ow_static'.DS);
-define('OW_URL_STATIC', OW_URL_HOME.'ow_static/');
-define('OW_URL_USERFILES', OW_URL_HOME.'ow_userfiles/');
 define('OW_DIR_PLUGINFILES', OW_DIR_ROOT.'ow_pluginfiles/');
-
-define('OW_PASSWORD_SALT', 'G1mOLurEMU51ZUSe');
-
+ 
+define('OW_DEBUG_MODE', false);
+define('OW_DEV_MODE', false);
+define('OW_INSTALL_PLUGINS', false);
+define('OW_PASSWORD_SALT', 'oxwall_salt_SK750');
+define('OW_PROFILER_ENABLE', false);
+ 
 define('OW_DIR_CORE', OW_DIR_ROOT.'ow_core'.DS);
 define('OW_DIR_INC', OW_DIR_ROOT.'ow_includes'.DS);
 define('OW_DIR_LIB', OW_DIR_ROOT.'ow_libraries'.DS);
@@ -26,22 +18,5 @@ define('OW_DIR_PLUGIN', OW_DIR_ROOT.'ow_plugins'.DS);
 define('OW_DIR_THEME', OW_DIR_ROOT.'ow_themes'.DS);
 define('OW_DIR_SYSTEM_PLUGIN', OW_DIR_ROOT.'ow_system_plugins'.DS);
 define('OW_DIR_SMARTY', OW_DIR_ROOT.'ow_smarty'.DS);
-
+ 
 define('OW_USE_CLOUDFILES', false);
-
-if ( defined('OW_CRON') )
-{
-    define('OW_DEBUG_MODE', false);
-    define('OW_DEV_MODE', false);
-    define('OW_PROFILER_ENABLE', false);
-}
-else
-{
-    /**
-    * Make changes in this block if you want to enable DEV mode and DEBUG mode
-    */
-
-    define('OW_DEBUG_MODE', false);
-    define('OW_DEV_MODE', false);
-    define('OW_PROFILER_ENABLE', false);
-}
